c71454b8902817b26ddb0af06b2ccb93dda19d7c
NikhilBtc <nikhilkishore4220@gmail.com>
2026-07-30 22:24:51 +0530

    Move Experience section before Projects

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Skills from './components/Skills'
-import Projects from './components/Projects'
 import Experience from './components/Experience'
+import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 
@@ -17,8 +17,8 @@ function App() {
       <Hero />
       <About />
       <Skills />
-      <Projects />
       <Experience />
+      <Projects />
       <Contact />
       <Footer />
     </>
